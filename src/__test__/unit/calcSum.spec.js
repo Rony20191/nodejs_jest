d6bcc4d213f031122e28fc16f09a83b978cb213b
Rony20191 <ronieresilva18@gmail.com>
2022-02-26 18:02:35 -0300

    first commit

---
+++ src/__test__/unit/calcSum.spec.js
@@ -0,0 +1,13 @@
+describe('Deve calcular a soma de duas variáveis', ()=>{
+
+   it('I need correct in sum ', ()=> {
+     const total = 11
+     const a = 5
+     const b = 6
+     let sum = a + b
+     expect(sum).toBe(total)
+   })
+
+   it('')
+
+})
